79af5160a565ce568cc574ed22b08146e9826c5c
Michael Agung <michaelputra.agung@gmail.com>
2021-07-05 11:53:45 +0700

    fix tabs

--- humiditySensor.py
+++ humiditySensor.py
@@ -11,11 +11,10 @@ def append_list_as_row(file_name, data,keys):
     # Open file in append mode
     if not os.path.exists(file_name):
         with open(file_name, "w") as f:
-	    writer = csv.DictWriter(f,fieldnames=keys)
+            writer = csv.DictWriter(f,fieldnames=keys)
             writer.writeheader()
-	    writer.writerows(data)
+            writer.writerows(data)
     else:
-
         with open(file_name, 'a+', newline='') as write_obj:
             csv_writer = csv.writer(write_obj)
             csv_writer.writerow(data)
@@ -24,11 +23,11 @@ def append_list_as_row(file_name, data,keys):
 keys=["datetime","temperature","humidity"]
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        timenow=datetime.now()
-	datapoint={"datetime":timenow,"temperature":temperature,"humidity":humidity
-        append_list_as_row("test.csv",datapoint,keys)
-        print(timenow,data)
-    else:
-        print("Sensore failure, Check wiring.");
-    time.sleep(5);
+        if humidity is not None and temperature is not None:
+            timenow=datetime.now()
+            datapoint={"datetime":timenow,"temperature":temperature,"humidity":humidity
+            append_list_as_row("test.csv",datapoint,keys)
+            print(timenow,data)
+        else:
+            print("Sensore failure, Check wiring.");
+        time.sleep(5);
